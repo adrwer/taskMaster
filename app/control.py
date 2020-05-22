e91196a6e0bccf49d6e67f259012e0326b55e53a
Adrian Werimo <adrwer7@gmail.com>
2020-05-22 15:48:15 +0300

    Reorganized file structure and add task logic

---
+++ app/control.py
@@ -0,0 +1,21 @@
+from flask import render_template, url_for, request, redirect
+from datetime import datetime
+from app.todo import Todo
+from app import app, db
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue with adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
